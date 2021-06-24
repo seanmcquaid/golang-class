ca9fb553bdec0feb0db58de72916da84cf5fa2f8
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-23 21:51:45 -0400

    learned about int vars

--- section-02/firstapp.go
+++ section-02/firstapp.go
@@ -8,6 +8,14 @@ func main(){
 
 	greetingText := "First app here"
 
+	luckyNumber := 12
+	evenMoreLuckyNumber := luckyNumber + 5
+
 	fmt.Println(greetingText)
-	fmt.Println(greetingText)
+	fmt.Println(luckyNumber)
+	fmt.Println(evenMoreLuckyNumber)
+
+	evenMoreLuckyNumber = luckyNumber * 3
+
+	fmt.Println(evenMoreLuckyNumber)
 }
