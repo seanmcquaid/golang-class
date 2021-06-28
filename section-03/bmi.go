d4b8b370223a33d0ab0a94e734b0a03bd2d34ae5
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-27 20:59:24 -0400

    added requirements

--- section-03/bmi.go
+++ section-03/bmi.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main(){
 	fmt.Println("Starting app")
+	// Output information
+	// Prompt for user input (Height + weight)
+	// Save that user input in variables
+	// Calculate the BMI (weight / (height * height))
+	// Output the calculated BMI
 }
