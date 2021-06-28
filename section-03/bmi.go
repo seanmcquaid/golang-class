0205445cd599b97610cb71efc01be67fe8ccaec3
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-27 21:02:32 -0400

    added outputs

--- section-03/bmi.go
+++ section-03/bmi.go
@@ -5,7 +5,11 @@ import "fmt"
 func main(){
 	fmt.Println("Starting app")
 	// Output information
+	fmt.Println("BMI Calculator")
+	fmt.Println("---------------------")
 	// Prompt for user input (Height + weight)
+	fmt.Println("Please enter your weight (kg): ")
+	fmt.Println("Please enter your height (m): ")
 	// Save that user input in variables
 	// Calculate the BMI (weight / (height * height))
 	// Output the calculated BMI
