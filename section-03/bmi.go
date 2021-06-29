b5ba85414d0688d4f378abce567485953c2fabc5
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-29 19:48:00 -0400

    cleaning inputs

--- section-03/bmi.go
+++ section-03/bmi.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var reader = bufio.NewReader(os.Stdin)
@@ -21,6 +23,13 @@ func main(){
 
 	fmt.Println("Please enter your height (m): ")
 	heightInput, _ := reader.ReadString('\n')
+
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+
 
 	// Save that user input in variables
 	// Calculate the BMI (weight / (height * height))
