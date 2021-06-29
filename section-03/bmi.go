6aaaf61bc18021683dd375b8729f6c7ba3e0704b
seanmcquaid <sean.mcquaid2@verizon.net>
2021-06-29 19:42:17 -0400

    grabbing user inputs

--- section-03/bmi.go
+++ section-03/bmi.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
 
 func main(){
 	fmt.Println("Starting app")
@@ -9,7 +15,13 @@ func main(){
 	fmt.Println("---------------------")
 	// Prompt for user input (Height + weight)
 	fmt.Println("Please enter your weight (kg): ")
+
+	// when enter is pressed, grab the input
+	weightInput, _ := reader.ReadString('\n')
+
 	fmt.Println("Please enter your height (m): ")
+	heightInput, _ := reader.ReadString('\n')
+
 	// Save that user input in variables
 	// Calculate the BMI (weight / (height * height))
 	// Output the calculated BMI
