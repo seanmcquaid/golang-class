f112148d34127dc948060ed9c8800dc064cdfdfd
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-01 15:48:07 -0400

    func example with types

--- section-04/functions.go
+++ section-04/functions.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("I am executing in a function")
+	a := 5
+	b := 10
+
+	sum := add(a, b)
+	fmt.Println(sum)
+}
+
+func add(a int, b int) int{
+	return a + b
 }
