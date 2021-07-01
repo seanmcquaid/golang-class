114a0206b05514d481d92b5f5d2b61219862119b
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-01 16:07:49 -0400

    additional func for printing

--- section-04/functions.go
+++ section-04/functions.go
@@ -7,9 +7,13 @@ func main(){
 	b := 10
 
 	sum := add(a, b)
-	fmt.Println(sum)
+	printNumber(sum)
 }
 
-func add(a int, b int) int{
+func add(a int, b int) int {
 	return a + b
+}
+
+func printNumber(num int){
+	fmt.Printf("The number is %v", num)
 }
