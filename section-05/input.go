a0517bd4ca41eca1d81d752b0c23c5db9bf6e558
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-03 10:12:56 -0400

    refactored input to avoid code duplication

--- section-05/input.go
+++ section-05/input.go
@@ -13,21 +13,17 @@ import (
 var reader = bufio.NewReader(os.Stdin)
 
 func getUserMetrics() (weight float64, height float64){
-	// Prompt for user input (Height + weight)
-	fmt.Println(info.WeightPrompt)
-
-	// when enter is pressed, grab the input
-	weightInput, _ := reader.ReadString('\n')
-
-	fmt.Println(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n')
-
-	// Save that user input in variables
-	weightInput = strings.Replace(weightInput, "\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\n", "", -1)
-
-	weight, _ = strconv.ParseFloat(weightInput, 64)
-	height, _ = strconv.ParseFloat(heightInput, 64)
+	weight = getUserInput(info.WeightPrompt)
+	height = getUserInput(info.HeightPrompt)
 
 	return
+}
+
+func getUserInput(promptText string) (enteredValue float64) {
+	fmt.Println(promptText)
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\n", "", -1)
+	enteredValue, _ = strconv.ParseFloat(userInput, 64)
+	
+	return 
 }
