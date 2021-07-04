0974fdb52c9e8352a2fdf4ae0232b39c386f605c
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-04 13:02:16 -0400

    function example for pointers

--- section-06/pointers.go
+++ section-06/pointers.go
@@ -14,4 +14,14 @@ func main() {
 	fmt.Println(myAge)
 	fmt.Println(*myAge)
 	fmt.Println(age)
+
+	doubledAge := double(age)
+	fmt.Println(doubledAge)
+	fmt.Println(age)
+}
+
+func double(num int) int {
+	result := num * 2
+	num = 100
+	return result
 }
