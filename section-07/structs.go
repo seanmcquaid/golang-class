5bcaff0a750a211aa702f8272dda84a3b1aceef7
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-06 20:16:24 -0400

    finished structs section

--- section-07/structs.go
+++ section-07/structs.go
@@ -6,18 +6,47 @@ import (
 	"os"
 	"runtime"
 	"strings"
+	"time"
 )
+
+type User struct {
+	firstName   string
+	lastName    string
+	birthDate   string
+	createdDate time.Time
+}
+
+func (user *User) outputDetails() {
+	fmt.Printf("My name is %v %v (born on %v)", user.firstName, user.lastName, user.birthDate)
+}
+
+func NewUser(fName string, lName string, bDate string) *User {
+	created := time.Now()
+
+	user := User{
+		firstName:   fName,
+		lastName:    lName,
+		birthDate:   bDate,
+		createdDate: created,
+	}
+
+	return &user
+}
 
 var reader = bufio.NewReader(os.Stdin)
 
 func main() {
+	var newUser *User
+
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
+	newUser = NewUser(firstName, lastName, birthdate)
+
 	// ... do something awesome with that gathered data!
 
-	fmt.Println(firstName, lastName, birthdate)
+	newUser.outputDetails()
 }
 
 func getUserData(promptText string) string {
@@ -32,5 +61,4 @@ func getUserData(promptText string) string {
 		cleanedInput = strings.Replace(userInput, "\n", "", -1)
 	}
 	return cleanedInput
-
 }
